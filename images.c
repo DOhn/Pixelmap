60489b63dabc4c0986e1f8533fbad1a88853af0d
Daniel Ohn <dao48@nau.edu>
2016-09-13 23:38:01 -0700

    Add new functions.  Add functionality to sameType function.

--- images.c
+++ images.c
@@ -7,7 +7,15 @@
 
 // Pixel *pixmap1d
 
-void sameType(input, output, type) {
+void sameType(FILE *fh, FILE *out, char* get) {
+	while(1) {
+		get = fgets(get, 100, fh);
+		fputs(get, out);
+
+		if (get == NULL) {
+			break;
+		}
+	}
 }
 
 void convertP3(input, output) {
@@ -19,14 +27,14 @@ void convertP6(input, output) {
 }
 
 int main(int argc, char *argv[]) {
+	// Checks if correct amount of arugments are inserted.
 	if (argc != 4) {
 		printf("Error: Incorrect amount of arguments.  Three is needed.\n");
 		return(1);
 	}
 
-	printf("%s\n", argv[1]);
-
-	if (argv[1] != "P6 " || argv[1] != "P3") {
+	// Checks if first argument is valid.
+	if (strncmp(argv[1], "P6", 2) == 1 || strncmp(argv[1], "P3", 2) == 1) {
 		printf("Error: First Argument must be in P3 or P6.\n");
 		return(1);
 	}
@@ -37,7 +45,8 @@ int main(int argc, char *argv[]) {
 	FILE *fh;
 	fh = fopen(argv[2], "r");
 
-	int i, size, color;
+	int i, color;
+	int op_type;
 	char type[2];
 
 	if(fh == NULL) {
@@ -49,27 +58,39 @@ int main(int argc, char *argv[]) {
 	
 	while(1) {
 		get = fgets(get, 100, fh);
+		
 		if (strcmp(get, "P3\n") == 0 || strcmp(get, "P6\n") == 0) {
 			type[0] = get[0];
 			type[1] = get[1];
 			if (strncmp(type, argv[1], 2) == 0) {
 				fputs(get, out);
+				fputs("\n", out);
+				get = fgets(get, 100, fh);
+				op_type = 0;
 			} 
 			else {
 				fputs(argv[1], out);
+				fputs("\n", out);
+				get = fgets(get, 100, fh);
+				op_type = 1;
 			}
 		}
+
 		if (get[0] == '#') {
 			get = fgets(get, 100, fh);
 		}
 		else {
-			printf("%s", get);
+			fputs(get, out);
 		}
 
 		if (strcmp(get, "255\n") == 0) {
 			color = atoi(get);
 			break;
 		}
+	}
+
+	if (op_type == 0) {
+		sameType(fh, out, get);
 	}
 
 	// printf("%c%c\n", type[0], type[1]);
